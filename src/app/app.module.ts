8fc2cecd791319987676e5a0a51b40e33f2a8e2d
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-03 16:52:37 -0300

    generate products-grid component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { ProductsGridComponent } from './products-grid/products-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    ProductsGridComponent
   ],
   imports: [
     BrowserModule,
