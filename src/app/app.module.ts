5ca366005f6ed5055c476dc18bed13016aa58bea
JesseSousa <rhadamesjesse14rj@gmail.com>
2022-06-03 16:56:06 -0300

    generate products-container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ProductsGridComponent } from './products-grid/products-grid.component';
+import { ProductsContainerComponent } from './products-container/products-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    ProductsGridComponent
+    ProductsGridComponent,
+    ProductsContainerComponent
   ],
   imports: [
     BrowserModule,
